83c4c2f0f96cdf9a354395650c78df60e00fc6ea
user <volosnikov.ia@yandex.ru>
2026-01-18 13:07:28 +0500

    Created other projects and added scalar api

--- NirUsb.Api/Program.cs
+++ NirUsb.Api/Program.cs
@@ -1,26 +1,24 @@
+using Scalar.AspNetCore;
+
 namespace NirUsb.Api;
 
-public class Program {
-    public static void Main(string[] args) {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
+public static class Program {
+    public static async Task Main(string[] args) {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment()) {
             app.MapOpenApi();
+            app.MapScalarApiReference();
         }
 
         app.UseAuthorization();
-
         app.MapControllers();
 
-        app.Run();
+        await app.RunAsync();
     }
 }
